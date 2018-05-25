1b12f921129b0e077e55fd4a4add98a6e6c56118
sunyinge0902 <871283020@qq.com>
2018-05-25 19:57:37 +0800

    vue+webpack

---
+++ src/todo/header.vue
@@ -0,0 +1,20 @@
+<template>
+	<header class="main-header">
+		<h1>STodo</h1>
+	</header>
+</template>
+
+<script>
+</script>
+
+<style lang="stylus" scoped>
+.main-header{
+	text-align center
+	h1{
+		font-size 100px
+		color rgba(175,47,47,0.4)
+		font-weight 100
+		margin 20px
+	}
+}
+</style>
